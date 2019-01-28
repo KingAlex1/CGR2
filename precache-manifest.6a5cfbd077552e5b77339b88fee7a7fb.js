c57708833b553899123fbb8d2264ba6bb77537a7
KingAlex1 <alex-sert2015@mail.ru>
2019-01-28 18:14:18 +0300

    CGR1

---
+++ precache-manifest.6a5cfbd077552e5b77339b88fee7a7fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "16dea417c3ae1bbee8e3a3a87f8ae0b5",
+    "url": "./static/media/ssgost.16dea417.jpg"
+  },
+  {
+    "revision": "5ab9eb64ad7982b6c837",
+    "url": "./static/css/main.01f9aaa0.chunk.css"
+  },
+  {
+    "revision": "5364f9f5ba0f11882722",
+    "url": "./static/js/1.5364f9f5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3cf9e75103ed607154f4930d71c49d36",
+    "url": "./static/media/dstrts.3cf9e751.jpg"
+  },
+  {
+    "revision": "f3bdfc7e267a01a984ccdfe6aedc341c",
+    "url": "./static/media/sstrts.f3bdfc7e.jpg"
+  },
+  {
+    "revision": "5ab9eb64ad7982b6c837",
+    "url": "./static/js/main.5ab9eb64.chunk.js"
+  },
+  {
+    "revision": "60f6279fb24158d5dc68096965e71199",
+    "url": "./static/media/tu.60f6279f.png"
+  },
+  {
+    "revision": "ffd29d3a6358ff70b79b8f96649500e7",
+    "url": "./static/media/gum-2768178_1920.ffd29d3a.jpg"
+  },
+  {
+    "revision": "44119fc9390566f2db00fec74f846f13",
+    "url": "./static/media/1.44119fc9.jpg"
+  },
+  {
+    "revision": "d442d5ca8a4b598b569a8dd43e27438d",
+    "url": "./static/media/moscow-1029511_1920.d442d5ca.jpg"
+  },
+  {
+    "revision": "10cf356284524a57cd0ae0db99913a8d",
+    "url": "./index.html"
+  }
+];
